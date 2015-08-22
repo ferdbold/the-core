c019090562914e9bdd8ac89c00bbef940c2ab660
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-08-21 22:04:34 -0400

    Initial scene composition

---
+++ ludumdare33/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+        ApplyMove();
+	}
+
+    private void ApplyMove() {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        if (horizontalAxis != 0) {
+            
+        }
+    }
+}
