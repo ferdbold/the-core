ca97dfe28ebbb54afd6371ec4c31c946089b36b6
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-08-22 22:05:46 -0400

    Added target animation and spawning

---
+++ ludumdare33/Assets/Scripts/Environment/Target.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    /* ATTRIBUTES */
+
+    private float _lifeSpan;
+    private float _timeElapsed;
+
+    /* COMPONENTS */
+
+    private Animation _animation;
+
+    /* CONSTRUCTOR */
+
+    void Awake() {
+        FindComponents();
+    }
+
+    private void FindComponents() {
+        _animation = GetComponent<Animation>();
+    }
+
+    /* METHODS */
+
+    void Update() {
+        _timeElapsed += Time.deltaTime;
+
+        if (_timeElapsed > _lifeSpan) {
+            GameObject.Destroy(gameObject, 0.35f);
+            _animation.Play();
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (LayerMask.NameToLayer("Projectile") == other.gameObject.layer) {
+            Debug.Log("Hit");
+        }
+    }
+
+    /* COMPONENTS */
+
+    public float LifeSpan {
+        get { return _lifeSpan; }
+        set { _lifeSpan = value; }
+    }
+}
--- ludumdare33/Assets/Scripts/Game/GameMode.cs
+++ ludumdare33/Assets/Scripts/Game/GameMode.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Timers;
 
 public class GameMode : MonoBehaviour {
 
     /* PUBLIC ATTRIBUTES */
 
-    public float _timeLeft = 30.0f;
+    public float timeLeft = 30.0f;
+
+    [Header("Target Spawn")]
+    public GameObject targetPrefab;
+    public float targetSpawnInterval = 2.0f;
+    public float targetLifeSpan = 5.0f;
+    public float[] heights;
 
     /* COMPONENTS */
 
     private HUD _hud;
+
+    /* ATTRIBUTES */
+
+    private float _timeUntilSpawn = 0;
 
     /* CONSTRUCTOR */
 
@@ -25,20 +36,53 @@ public class GameMode : MonoBehaviour {
 
 	void Update() {
         TickClock();
+        TickSpawn();
 	}
 
     /// <summary>
     /// Tick the game clock.
     /// </summary>
     private void TickClock() {
-        _timeLeft -= Time.deltaTime;
-        _hud.SetClock(_timeLeft);
+        this.timeLeft -= Time.deltaTime;
+        _hud.SetClock(this.timeLeft);
 
-        if (_timeLeft < 0) {
+        if (this.timeLeft < 0) {
             EndGame();
         }
     }
 
+    /// <summary>
+    /// Tick the target spawning clock.
+    /// </summary>
+    private void TickSpawn() {
+        _timeUntilSpawn += Time.deltaTime;
+
+        if (_timeUntilSpawn > this.targetSpawnInterval) {
+            _timeUntilSpawn = 0;
+            SpawnTarget();
+        }
+    }
+
+    /// <summary>
+    /// Spawn a target around the core.
+    /// </summary>
+    private void SpawnTarget() {
+        // Randomize target position
+        float targetHeight = this.heights[Random.Range(0, this.heights.Length)];
+        Vector3 targetPos = new Vector3(0, targetHeight, 0);
+        Quaternion targetRot = Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0);
+
+        // Create new target
+        GameObject newTargetObj = (GameObject)GameObject.Instantiate(this.targetPrefab, targetPos, targetRot);
+        Target newTarget = newTargetObj.GetComponent<Target>();
+        
+        // Initialize new target
+        newTarget.LifeSpan = this.targetLifeSpan;
+    }
+
+    /// <summary>
+    /// End the game.
+    /// </summary>
     private void EndGame() {
 
     }
