b60c607761569012b7066b64d7187da1b66125e2
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-08-22 22:39:37 -0400

    Better clock feedback

--- ludumdare33/Assets/Scripts/UI/ClockWidget.cs
+++ ludumdare33/Assets/Scripts/UI/ClockWidget.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
 public class ClockWidget : MonoBehaviour {
 
@@ -9,6 +10,10 @@ public class ClockWidget : MonoBehaviour {
     private Text _timeLeftText;
     private RectTransform _dotPivot;
 
+    /* ATTRIBUTES */
+
+    private int _lastTimeRecorded;
+
     /* CONSTRUCTOR */
 
     void Awake() {
@@ -16,18 +21,29 @@ public class ClockWidget : MonoBehaviour {
     }
 
     private void FindComponents() {
-        _timeLeftText = GetComponent<Text>();
+        _timeLeftText = transform.Find("TimeLeft").GetComponent<Text>();
         _dotPivot = transform.Find("DotPivot").GetComponent<RectTransform>();
+    }
+
+    private void PlayTickingEmphasis() {
+        _timeLeftText.rectTransform.localScale = new Vector3(1.8f, 1.8f, 1.8f);
+        _timeLeftText.rectTransform.DOScale(new Vector3(1.0f, 1.0f, 1.0f), 0.4f);
     }
 
     /* PROPERTIES */
 
     public float TimeLeft {
         set {
+            if (_lastTimeRecorded != (int)value) {
+                PlayTickingEmphasis();
+            }
+
             float decimalPart = value - Mathf.Floor(value);
 
             _timeLeftText.text = Mathf.Ceil(value).ToString();
             _dotPivot.rotation = Quaternion.Euler(0, 0, decimalPart * 360f);
+
+            _lastTimeRecorded = (int)value;
         }
     }
 }
